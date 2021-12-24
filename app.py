1312a67b518d4e98e79f7fd43ad39eef2e8c261f
AAbdurakhmanov <aabdurakhmanov98@gmail.com>
2021-12-24 21:41:56 +0500

    change

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from pytube import YouTube
 
 dock = tk.Tk()
 dock.geometry('800x400')
-dock.resizable(0,0)
+dock.resizable(0, 0)
 dock.title("Youtube video downloader")
 tk.Label(dock, text='Youtube Video Downloader', font='arial 20 bold').pack()
 
@@ -15,31 +15,32 @@ link = tk.StringVar()
 
 tk.Label(dock,
          text="Paste Link Here: ",
-         font="arial 20 bold")\
-            .place(x=160, y=60)
+         font="arial 20 bold") \
+    .place(x=160, y=60)
 
 link_error = tk.Entry(dock,
                       width=70,
-                      textvariable=link)\
-                    .place(x=32, y=90)
+                      textvariable=link) \
+    .place(x=32, y=90)
 
-#Function to download the video
+
+# Function to download the video
 def Downloader():
-    url =YouTube(str(link.get()))
-    video =url.streams.first()
+    url = YouTube(str(link.get()))
+    video = url.streams.first()
     video.download()
     tk.Label(dock,
-             text ="Successfully Downloaded",
-             font ="arial 15")\
-            .place(x =180, y =200)
+             text="Successfully Downloaded",
+             font="arial 15") \
+        .place(x=180, y=200)
 
 
-#Download Button
+# Download Button
 tk.Button(dock,
-          text ="DOWNLOAD",
-          font ="Verdana 15 bold",
-          bg ="orange", padx =2,
-          command =Downloader)\
-        .place(x=180, y=150)
+          text="DOWNLOAD",
+          font="Verdana 15 bold",
+          bg="orange", padx=2,
+          command=Downloader) \
+    .place(x=180, y=150)
 
 dock.mainloop()
